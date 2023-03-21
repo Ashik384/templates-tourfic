8460f9e9d2c3290dd5555a37b7bd50b47ca9f610
Ashik384 <ashikulislam384@gmail.com>
2023-03-21 16:20:48 +0600

    Marge With Tourfic Assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const minify = require('gulp-clean-css');
-const autoprefixer = require('gulp-autoprefixer');
+//const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 
 //SCSS
 function compileScss(){
-    return src('assets/sass/*.scss')
+    return src('assets/sass/app/css/free/*.scss')
     .pipe(sass())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
+    // .pipe(autoprefixer({
+    //     browsers: ['last 2 versions'],
+    //     cascade: false
+    // }))
     .pipe(dest('assets/css/unminified'))
 }
 // Minified CSS
 function minifyCssfile(){
     return src('assets/css/unminified/*.css')
     .pipe(minify())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
+    // .pipe(autoprefixer({
+    //     browsers: ['last 2 versions'],
+    //     cascade: false
+    // }))
     .pipe(rename({suffix:'.min'}))
     .pipe(dest('assets/css/minified'))
 }
